b95695c0eae18d93953ab77db3f397ca351898fc
Joaquin <jcieri126@alumnos.frh.utn.edu.ar>
2025-08-17 18:19:23 -0300

    Instalacion de Librerias

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
+import sys
+import time
+import numpy as np
+
+print("Proyecto Medición de Resistencias - Grupo 1")
+
+
+
+
+
+
 """
 Created on Sun Aug 17 17:29:13 2025
 
 @author: Joaquin Cieri
 """
-
-print("Proyecto Medición de Resistencias - Grupo 1")
