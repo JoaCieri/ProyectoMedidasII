892a7ea439c1632ea46357e3b2077bae4362008b
Joaquin <jcieri126@alumnos.frh.utn.edu.ar>
2025-08-17 21:42:09 -0300

    Agregado de interfaz y creacion de clase

--- src/main.py
+++ src/main.py
@@ -3,13 +3,29 @@ import sys
 import time
 import numpy as np
 
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QFileDialog
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
 print("Proyecto Medición de Resistencias - Grupo 1")
 
+class Ui(QMainWindow):
 
-
-
-
-
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Interfaz.ui', self)   
+            
+        # PARAMETROS DE LA INTERFAZ
+        # VARIABLES
+        # CONEXION DE BOTONES
+            
+            
+    def scanport(self):
+        print("Escanear puerto serie") 
+       
+    def conectarport(self):
+        print("Conectar puerto serie")
 """
 Created on Sun Aug 17 17:29:13 2025
 
